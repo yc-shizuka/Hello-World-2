72d2932fdc109632b4716d792188d1a4c60bf77e
Yoshihide Chubachi <yoshi@chubachi.net>
2011-08-26 23:21:09 +0900

    aueo

--- HelloWorld2/src/Main.java
+++ HelloWorld2/src/Main.java
@@ -2,6 +2,6 @@
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World 2");
-		System.out.println("Hello World, again");
+		System.out.println("Hello World 2, again");
 	}
 }
