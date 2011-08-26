6e70cf30d9b2eb63cefa6159e5ac6443b93ae362
Yoshihide Chubachi <yoshi@chubachi.net>
2011-08-27 00:24:49 +0900

    Shizuka 2

--- HelloWorld2/src/Main.java
+++ HelloWorld2/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello World 2");
 		System.out.println("Hello World 2, again");
-		System.out.printls("I am Shizuka.");
+		System.out.println("I am Shizuka.");
 	}
 }
